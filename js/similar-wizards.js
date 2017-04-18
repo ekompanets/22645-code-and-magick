402bdc9b25b5f8724ccac69d834bad13478a9adf
ekompanets <amelie.kom@gmail.com>
2017-04-18 23:20:17 +0300

    incapsulation

---
+++ js/similar-wizards.js
@@ -0,0 +1,30 @@
+'use strict';
+
+ window.similarWizards = (function () {
+  
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+ 
+  // отрисовка магами
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.fullName;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyeColor;
+
+    return wizardElement;
+  };
+
+  var renderWizardsBlock = function (num) {
+    var wizards = createWizardSet(num);
+    var similarListElement = document.querySelector('.setup-similar-list');
+    var fragment = document.createDocumentFragment();
+    // рисуем магов
+    for (var i = 0; i < num; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+  }
+  
+  return renderWizardsBlock;
+})();
